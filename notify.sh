3d6d1c982167f4fb5eedf1f414974ec999c7b5cb
levy <897895407@qq.com>
2019-07-29 18:07:20 +0800

    chore: fix notify.sh

--- notify.sh
+++ notify.sh
@@ -13,7 +13,7 @@ curl -H "Authorization: token $GITHUB_TOKEN" $url > $resp_tmp_file
 html_url=`cat $resp_tmp_file | sed -n 5p | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}'`
 body=`cat $resp_tmp_file | grep body | sed 's/\"body\"://g;s/\"//g'`
 
-msg='{"msgtype": "markdown", "markdown": {"title": "v-countdown更新", "text": "@所有人\n# [count-down]('$html_url')\n'$body'"}}'
+msg='{"msgtype": "markdown", "markdown": {"title": "count-down更新", "text": "@所有人\n# [count-down]('$html_url')\n'$body'"}}'
 
 curl -X POST https://oapi.dingtalk.com/robot/send\?access_token\=$DINGTALK_ROBOT_TOKEN -H 'Content-Type: application/json' -d "$msg"
 
